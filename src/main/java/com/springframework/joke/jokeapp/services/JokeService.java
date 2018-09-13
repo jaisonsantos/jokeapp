b5d50a43d2c08d8194e001b5ef66375454e81580
Jaison Santos <jaison_js@hotmail.com>
2018-09-13 17:27:47 +0100

    Adding Joke Service

---
+++ src/main/java/com/springframework/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,8 @@
+package com.springframework.joke.jokeapp.services;
+/**
+ * Created by Jaison 09/13/2018
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/springframework/joke/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.springframework.joke.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jaison 09/13/2018
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
