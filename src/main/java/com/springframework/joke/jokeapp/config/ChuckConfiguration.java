03ab982566b263eaa8fd13a0764e4d572fa643f0
Jaison Santos <jaison_js@hotmail.com>
2018-09-19 20:27:04 +0100

    Java Configuration Component Scan, Banner initializing.

---
+++ src/main/java/com/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,19 @@
+package com.springframework.joke.jokeapp.config;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Jaison 09/19/2018
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+
+}
